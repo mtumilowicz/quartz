e9c06a8639e7b75f5a6b21077691b77b218501b6
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-07-22 20:32:02 +0200

    removing unnecessary code

--- src/main/java/com/example/quartz/QuartzApplication.java
+++ src/main/java/com/example/quartz/QuartzApplication.java
@@ -1,31 +1,12 @@
 package com.example.quartz;
 
-import org.quartz.JobDetail;
-import org.quartz.SimpleScheduleBuilder;
-import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuartzApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuartzApplication.class, args);
-    }
-
-    @Bean
-    public Trigger sampleJobTrigger(JobDetail jobDetail) {
-        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder
-                .simpleSchedule()
-                .withIntervalInSeconds(2)
-                .repeatForever();
-
-        return TriggerBuilder
-                .newTrigger()
-                .forJob(jobDetail)
-                .withSchedule(scheduleBuilder)
-                .build();
     }
 }
